ca30c8e31969b0b3858a7ae1df0e639a6f77cfb4
wu-bacca <kroeze.wb@gmail.com>
2022-06-08 22:44:55 +0200

    Initial setup cognito.

--- lib/wk-cognito-infra-stack.ts
+++ lib/wk-cognito-infra-stack.ts
@@ -1,16 +1,24 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cognito from 'aws-cdk-lib/aws-cognito';
 
 export class WkCognitoInfraStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const userPool = new cognito.UserPool(this, 'wouterkroeze', {
+      userPoolName: 'wouterkroeze',
+      passwordPolicy: {
+        minLength: 8,
+        requireUppercase: true,
+        requireLowercase: true,
+        requireSymbols: true,
+        requireDigits: true,
+      }
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'WkCognitoInfraQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const frontendAppClient = new cognito.UserPoolClient(this, 'frontendAppClient', {
+      userPool: userPool,
+    });
   }
-}
+}``
